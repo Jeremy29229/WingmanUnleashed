598a79ce3691eebe72551e5a8537823f3362b76f
LAndrade1093 <landrade@student.neumont.edu>
2014-10-05 16:54:40 -0600

    Basic Camera Controls
    Very basic player and camera movement for the game. For now, the player
    moves using WASD as the camera follows the character. More advanced
    camera controls will be added later in the project. Also committing a
    text file with instructions on how to set up the camera and the
    controls.

---
+++ Camera_ThirdPerson.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_ThirdPerson : MonoBehaviour 
+{
+	public float distanceBehindObject;
+	public float distanceAboveObject;
+	public Transform targetObject;
+	public float movementSmoothing;
+
+	private static Camera_ThirdPerson Instance;
+
+	void Start () 
+	{
+		Instance = this;
+	}
+		
+	void LateUpdate () 
+	{
+		Vector3 abovePlayer = targetObject.up * distanceAboveObject;
+		Vector3 belowPlayer = targetObject.forward * distanceBehindObject;
+		Vector3 newPosition = targetObject.position + abovePlayer - belowPlayer;
+
+		transform.position = Vector3.Lerp (transform.position, newPosition, Time.deltaTime * movementSmoothing);
+
+		transform.LookAt (targetObject);
+	}
+	
+
+}
