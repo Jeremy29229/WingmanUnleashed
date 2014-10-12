3da7ef1acea3321ac0926c10e1beb81b9ea63e97
Tyler Dacus <tdacus@student.neumont.edu>
2014-10-11 21:41:30 -0600

    Added HUD canvas

--- WingmanUnleashed/Assets/Player.cs
+++ WingmanUnleashed/Assets/Player.cs
@@ -2,11 +2,13 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
+    private Canvas HUD;
 	private float detectionLevel;
 
 	// Use this for initialization
 	void Start () {
 		detectionLevel = 0f;
+        //HUD = (Canvas)GameObject.Find("HUD").GetType;
 	}
 	
 	// Update is called once per frame
