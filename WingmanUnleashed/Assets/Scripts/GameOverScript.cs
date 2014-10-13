94a03f38d5aa77941e5ca5ab08c56ea943687f4c
Taylor Garcia <tclustuff@gmail.com>
2014-10-13 10:41:03 -0600

    added game over screen

---
+++ WingmanUnleashed/Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameOverScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        gameObject.GetComponent<Canvas>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    
+	}
+    public void HideGameOver()
+    {
+        gameObject.GetComponent<Canvas>().enabled = false;
+    }
+    public void ShowGameOver(string message="The End")
+    {
+        Show(message);
+    }
+    public void ShowGameOverWin(string message = "You Win")
+    {
+        Show(message);
+    }
+    public void ShowGameOVerLose(string message = "You Lose")
+    {
+        Show(message);
+    }
+    private void Show(string message)
+    {
+        GameObject.Find("GameOverMessage").GetComponent<Text>().text = message;
+        gameObject.GetComponent<Canvas>().enabled = true;
+    }
+}
