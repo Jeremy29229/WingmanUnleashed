1859820903fea914304eae8ea9aeb7993a70ee9f
Tyler Dacus <tdacus@student.neumont.edu>
2014-10-21 10:39:59 -0600

    Adding Objective Menu :octopus: #Octopus4Lyfe

---
+++ WingmanUnleashed/Assets/ObjectiveDisplayScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ObjectiveDisplayScript : MonoBehaviour {
+
+    public GameObject ObjectiveBar;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void AddObjective(string name, string objectiveText)
+    {
+        var id = (GameObject)Instantiate(ObjectiveBar);
+        id.name = name + "Display";
+        id.transform.FindChild("Text").GetComponent<Text>().text = objectiveText;
+        id.transform.SetParent(GameObject.Find("ItemsDisplay").transform, false);
+    }
+
+    public void RemoveObjective(string name)
+    {
+        var id = GameObject.Find("ObjectiveBar").transform.FindChild(name + "Display");
+        Destroy(id);
+    }
+}
