6f861a4e29c18388b8249e5c3ccc62013be663d6
Tyler Dacus <tdacus@student.neumont.edu>
2014-10-27 11:47:18 -0600

    Updated Objective menu

--- WingmanUnleashed/Assets/ObjectiveDisplayScript.cs
+++ WingmanUnleashed/Assets/ObjectiveDisplayScript.cs
@@ -7,12 +7,21 @@ public class ObjectiveDisplayScript : MonoBehaviour {
     public GameObject ObjectiveBar;
 	// Use this for initialization
 	void Start () {
-	
+        gameObject.GetComponentInParent<Canvas>().enabled = false;
+        AddObjective("Start", "Talk to the client.");
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            gameObject.GetComponent<Canvas>().enabled = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.O))
+        {
+            gameObject.GetComponent<Canvas>().enabled = false;
+        }
 	}
 
     public void AddObjective(string name, string objectiveText)
@@ -20,7 +29,7 @@ public class ObjectiveDisplayScript : MonoBehaviour {
         var id = (GameObject)Instantiate(ObjectiveBar);
         id.name = name + "Display";
         id.transform.FindChild("Text").GetComponent<Text>().text = objectiveText;
-        id.transform.SetParent(GameObject.Find("ItemsDisplay").transform, false);
+        id.transform.SetParent(GameObject.Find("ScrollBounds").transform, false);
     }
 
     public void RemoveObjective(string name)
@@ -28,4 +37,6 @@ public class ObjectiveDisplayScript : MonoBehaviour {
         var id = GameObject.Find("ObjectiveBar").transform.FindChild(name + "Display");
         Destroy(id);
     }
+
+
 }
