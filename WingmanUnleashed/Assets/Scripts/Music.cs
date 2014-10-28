e519e3f27e18263c328752aa1bbc362c1c2a940e
DaleChetney <dalechetney@gmail.com>
2014-10-28 11:39:24 -0600

    put in the music

--- WingmanUnleashed/Assets/Scripts/Music.cs
+++ WingmanUnleashed/Assets/Scripts/Music.cs
@@ -2,8 +2,8 @@
 using System.Collections;
 
 public class Music : MonoBehaviour {
-    AudioSource player;
-    AudioClip wingman;
+    public AudioSource player;
+    public AudioClip wingman;
     // Use this for initialization
     void Start()
     {
