e5baa1a72f8ae62ce938e11bf46bd11e84df38a8
Jeremy Jones <jeremy29229@yahoo.com>
2014-10-30 01:30:44 -0600

    Added overhead displays part 1

--- WingmanUnleashed/Assets/Scripts/Collectable.cs
+++ WingmanUnleashed/Assets/Scripts/Collectable.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Collectable : MonoBehaviour, IInteractable
 {
@@ -8,17 +9,31 @@ public class Collectable : MonoBehaviour, IInteractable
 	public Sprite inventorySprite;
 	public int SellValue = 0;
 	public bool IsKeepableItem = false;
+	public bool IsImportantItem = false;
+	bool lastImportantItemState = false;
 
 	void Start()
 	{
+		GetComponentInChildren<Canvas>().enabled = IsImportantItem;
+		lastImportantItemState = IsImportantItem;
+
 		inventory = GameObject.Find(PlayerObjectName).GetComponent<Inventory>();
 		wingman = GameObject.Find(PlayerObjectName).GetComponent<Player>();
 		GetComponent<Interactable>().AdditionalInformation = "($" + SellValue + ")";
 	}
 
+	void Update()
+	{
+		if (IsImportantItem != lastImportantItemState)
+		{
+			GetComponentInChildren<Canvas>().enabled = IsImportantItem;
+			lastImportantItemState = IsImportantItem;
+		}
+	}
+
 	void IInteractable.InteractWith()
 	{
-        GameObject.Find("SoundManager").GetComponent<SoundManager>().PlaySoundAt("cashGrab", gameObject.transform.position);
+		GameObject.Find("SoundManager").GetComponent<SoundManager>().PlaySoundAt("cashGrab", gameObject.transform.position);
 		inventory.AddItem(gameObject.GetComponent<Interactable>().InteractableName, inventorySprite);
 		if (wingman.numDetectors > 0)
 		{
