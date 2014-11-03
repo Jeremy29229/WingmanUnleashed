d1d0d768683f03b0c63e944b129424eb4e197063
Taylor Garcia <tclustuff@gmail.com>
2014-11-03 03:14:16 -0700

    Bouncer animations

---
+++ WingmanUnleashed/Assets/TestManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        GameObject.Find("BouncerAngry1").GetComponent<CharacterAnimator>().StartDancingSamba();
+        GameObject.Find("BouncerAngry2").GetComponent<CharacterAnimator>().StartDancingSamba();
+        GameObject.Find("BouncerAngry3").GetComponent<CharacterAnimator>().StartDancingSamba();
+        GameObject.Find("BouncerAngry4").GetComponent<CharacterAnimator>().StartDancingSamba();
+        GameObject.Find("BouncerHappy1").GetComponent<CharacterAnimator>().StartDancingSamba();
+        GameObject.Find("BouncerHappy2").GetComponent<CharacterAnimator>().StartDancingSamba();
+        GameObject.Find("BouncerHappy3").GetComponent<CharacterAnimator>().StartDancingSamba();
+        GameObject.Find("BouncerHappy4").GetComponent<CharacterAnimator>().StartDancingSamba();
+        GameObject.Find("BouncerStoic1").GetComponent<CharacterAnimator>().StartDancingSamba();
+        GameObject.Find("BouncerStoic2").GetComponent<CharacterAnimator>().StartDancingSamba();
+        GameObject.Find("BouncerStoic3").GetComponent<CharacterAnimator>().StartDancingSamba();
+        GameObject.Find("BouncerStoic4").GetComponent<CharacterAnimator>().StartDancingSamba();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    
+	}
+}
