d07761ecb3e697790f7d34075917c8e8e1b19732
Tyler Dacus <tdacus@student.neumont.edu>
2014-11-11 23:47:18 -0700

    Private balcony jank :japanese_goblin:

---
+++ WingmanUnleashed/Assets/Scripts/RooftopJankMagic.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RooftopJankMagic : MonoBehaviour {
+    public GameObject Client;
+    public GameObject Target;
+    public GameObject Wingman;
+    public GameObject ClientWaypoint;
+    public GameObject TargetWaypoint;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Client.GetComponent<Commandable>().stopFollowing();
+        Target.GetComponent<Commandable>().stopFollowing();
+        Client.GetComponent<Commandable>().teleport(ClientWaypoint.transform.position);
+        Client.GetComponent<Commandable>().teleport(TargetWaypoint.transform.position);
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+    }
+
+}
