407c3cd70f1a884fd18578bf7bed6786d4ffb619
Taylor Garcia <tclustuff@gmail.com>
2014-11-12 01:06:32 -0700

    :confetti_ball: :fire: :foggy: :astonished:

---
+++ WingmanUnleashed/Assets/Scripts/PartyMachineScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PartyMachineScript : MonoBehaviour {
+
+    public float EffectDuration = 10;
+    public float IntermissionBetweenEffects = 1;
+    public ParticleSystem[] PartyEffects;
+    private ParticleSystem currentEffect;
+	// Use this for initialization
+	void Start () 
+    {
+        if (PartyEffects != null && PartyEffects.Length > 0)
+        {
+            for (int i = 0; i < PartyEffects.Length; i++)
+            {
+                PartyEffects[i].Stop();
+            }
+            StartRandomEffect();
+        }
+
+	}
+
+    private void StartRandomEffect()
+    {
+        bool foundRandomEffect = false;
+        while (!foundRandomEffect)
+        {
+            int index = Random.Range(0, PartyEffects.Length);
+            if (PartyEffects[index] != currentEffect)
+            {
+                foundRandomEffect = true;
+                currentEffect = PartyEffects[index];
+                currentEffect.Play();
+            }
+        }
+        StartCoroutine("WaitForEffectDuration");
+    }
+    
+    
+
+    private IEnumerator WaitForEffectDuration()
+    {
+        yield return new WaitForSeconds(EffectDuration);
+        if (currentEffect != null)
+        {
+            currentEffect.Stop();
+        }
+        yield return new WaitForSeconds(IntermissionBetweenEffects);
+        StartRandomEffect();
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
